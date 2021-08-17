817f2d0d33b46af3df0f20a5f605537b2232a157
Htet Lin Aung <jokejoke186@gmail.com>
2021-08-17 20:58:26 +0630

    lesson-2

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Post Lists</h1>
+  <div v-for="post in posts" :key="post.id">
+     <SinglePost :post="post"> </SinglePost>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+export default {
+    components:{SinglePost},
+    props:["posts"],
+    setup(props){
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
